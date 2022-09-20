523fafce9c3958f592478ea27ea6b703bda2127b
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-09-20 18:22:15 +0200

    added jobs filter on jobs list view

--- backend_core/jobs/views.py
+++ backend_core/jobs/views.py
@@ -18,7 +18,7 @@ class JobsListView(FilterView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['jobs'] = list(chain(models.StudioSession.objects.all(), models.Concert.objects.all(),
-                                     models.Tour.objects.all()))
+                               models.Tour.objects.all()))
         return context
 
 
@@ -34,23 +34,3 @@ class JobDetailView(DetailView):
         context['job_detailed'] = [job for job in jobs if job.title == context['job'].title][0]
         context['owner_profile'] = UserProfile.objects.get(user_id=context['job'].owner_id)
         return context
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #
-    #     # if not models.StudioSession.objects.get(job_ptr_id=context['job'].id):
-    #     #     if not models.Concert.objects.get(job_ptr_id=context['job'].id):
-    #     #         if not models.Tour.objects.get(job_ptr_id=context['job'].id):
-    #     #             raise ValueError('No matching item!')
-    #     #         else:
-    #     #             context['job_detailed'] = models.Tour.objects.get(job_ptr_id=context['job'].id)
-    #     #     else:
-    #     #         context['job_detailed'] = models.Concert.objects.get(job_ptr_id=context['job'].id)
-    #     # else:
-    #     #     context['job_detailed'] = models.StudioSession.objects.get(job_ptr_id=context['job'].id)
-    #     if models.StudioSession.objects.get(job_ptr_id=context['job'].id):
-    #         context['job_detailed'] = models.StudioSession.objects.get(job_ptr_id=context['job'].id)
-    #     elif models.Concert.objects.get(job_ptr_id=context['job'].id):
-    #         context['job_detailed'] = models.Concert.objects.get(job_ptr_id=context['job'].id)
-    #
-    #     return context
