6d506db8b2ea109de9ac7fed21db83aadbcc0e48
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-09-20 16:39:49 +0200

    added job owner avatar to the job details page

--- backend_core/jobs/views.py
+++ backend_core/jobs/views.py
@@ -6,6 +6,7 @@ from django_filters.views import FilterView
 from . import models
 
 from .filters import JobsFilter
+from users.models import UserProfile
 
 
 class JobsListView(FilterView):
@@ -25,3 +26,31 @@ class JobDetailView(DetailView):
     model = models.Job
     template_name = 'jobs/job_details.html'
     context_object_name = 'job'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        jobs = list(chain(models.StudioSession.objects.all(), models.Concert.objects.all(),
+                                     models.Tour.objects.all()))
+        context['job_detailed'] = [job for job in jobs if job.title == context['job'].title][0]
+        context['owner_profile'] = UserProfile.objects.get(user_id=context['job'].owner_id)
+        return context
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #
+    #     # if not models.StudioSession.objects.get(job_ptr_id=context['job'].id):
+    #     #     if not models.Concert.objects.get(job_ptr_id=context['job'].id):
+    #     #         if not models.Tour.objects.get(job_ptr_id=context['job'].id):
+    #     #             raise ValueError('No matching item!')
+    #     #         else:
+    #     #             context['job_detailed'] = models.Tour.objects.get(job_ptr_id=context['job'].id)
+    #     #     else:
+    #     #         context['job_detailed'] = models.Concert.objects.get(job_ptr_id=context['job'].id)
+    #     # else:
+    #     #     context['job_detailed'] = models.StudioSession.objects.get(job_ptr_id=context['job'].id)
+    #     if models.StudioSession.objects.get(job_ptr_id=context['job'].id):
+    #         context['job_detailed'] = models.StudioSession.objects.get(job_ptr_id=context['job'].id)
+    #     elif models.Concert.objects.get(job_ptr_id=context['job'].id):
+    #         context['job_detailed'] = models.Concert.objects.get(job_ptr_id=context['job'].id)
+    #
+    #     return context
