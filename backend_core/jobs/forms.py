2cd3d1aef1a8d3fdbc202092436f89efdf431eeb
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-10-03 20:04:30 +0200

    improved forms for creating a new job

--- backend_core/jobs/forms.py
+++ backend_core/jobs/forms.py
@@ -1,56 +1,27 @@
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
 from django import forms
 from . import models
 
 
-# class CreateJobForm(forms.ModelForm):
-#     helper = FormHelper()
-#     helper.add_input(Submit('submit', 'Post', css_class='btn btn-danger'))
-#     helper.form_method = 'POST'
-
-
-# class CreateStudioSessionForm(CreateJobForm):
-#     class Meta:
-#         model = models.StudioSession
-#         exclude = ('owner', 'added', 'updated', 'is_active')
-#
-#
-# class CreateConcertForm(CreateJobForm):
-#     class Meta:
-#         model = models.Concert
-#         exclude = ('owner', 'added', 'updated', 'is_active')
-#
-#
-# class CreateTourForm(CreateJobForm):
-#     class Meta:
-#         model = models.Tour
-#         exclude = ('owner', 'added', 'updated', 'is_active')
-#
-#
-# class JobAccessForm(CreateJobForm):
-#     class Meta:
-#         model = models.JobAccess
-#         exclude = ('candidate', 'job')
-
-
-class CreateStudioSessionForm(forms.ModelForm):
+class CreateJobForm(forms.ModelForm):
     description = forms.CharField(widget=forms.Textarea(attrs={
-        'rows': 2,
+        'rows': 4,
+        'cols': 23,
     }))
 
+
+class CreateStudioSessionForm(CreateJobForm):
     class Meta:
         model = models.StudioSession
         exclude = ('owner', 'added', 'updated', 'is_active')
 
 
-class CreateConcertForm(forms.ModelForm):
+class CreateConcertForm(CreateJobForm):
     class Meta:
         model = models.Concert
         exclude = ('owner', 'added', 'updated', 'is_active')
 
 
-class CreateTourForm(forms.ModelForm):
+class CreateTourForm(CreateJobForm):
     class Meta:
         model = models.Tour
         exclude = ('owner', 'added', 'updated', 'is_active')
