a79796ca9730fdf2d132fc4e306e27ca7ff37960
Maciej Piszczek <maciej.piszczek93@gmail.com>
2022-12-19 20:40:07 +0100

    links to events in chat auto messages now are working

--- backend_core/static/js/event-links.js
+++ backend_core/static/js/event-links.js
@@ -4,9 +4,12 @@ autoMsgs.forEach(msg => {
     if (msg.textContent.includes('AUTO MESSAGE')) {
         let words = msg.textContent.split('"');
         if (words.length === 3) {
-            const eventTitle = `<a href="">` + words[1] + `</a>`;
-            words[1] = eventTitle;
+            let eventTitle = words[1];
+            let titleSlug = eventTitle.toLowerCase().replace(' ', '-');
+            const eventTitleHtml = `<a href="/jobs/` + titleSlug + `/">` + words[1] + `</a>`;
+            words[1] = eventTitleHtml;
             msg.innerHTML = words.join(" ");
+            console.log(titleSlug);
         }
     }
 })
