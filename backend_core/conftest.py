9ca7597b8e3f1dfb7a5a6d235ebc1767d5448256
Maciej Piszczek <maciej.piszczek93@gmail.com>
2023-03-27 18:22:46 +0200

    improved unit tests for jobs app

--- backend_core/conftest.py
+++ backend_core/conftest.py
@@ -1,6 +1,12 @@
+import datetime
+import random
+
 import pytest
+from _decimal import Decimal
+from django.contrib.auth.models import Group
 
 from chat.models import Room
+from jobs.models import Job, CutUnit
 from users.models import UserProfile
 
 
@@ -18,6 +24,25 @@ def get_user_profile(user):
     user_ = user
     profile = UserProfile.objects.get(user=user_)
     return profile
+
+
+@pytest.fixture
+def create_user_and_job(user):
+    user_ = user
+    user_.groups.add(Group.objects.get(name='musicians'))
+    now_ = datetime.datetime.now(tz=datetime.timezone.utc)
+    job = Job.objects.create(
+        owner=user_,
+        title='Rictusempra',
+        instrument='guitar',
+        music_style='rock',
+        description='loremipsum',
+        cut=Decimal(100),
+        cut_unit=random.choice(CutUnit.objects.all()),
+        event_start=now_ + datetime.timedelta(hours=1),
+        event_end=now_ + datetime.timedelta(hours=3),
+    )
+    return user_, job
 
 
 @pytest.fixture
--- backend_core/jobs/tests/test_jobs_forms.py
+++ backend_core/jobs/tests/test_jobs_forms.py
@@ -1,40 +1,6 @@
-import random
-
 import pytest
-import datetime
-
-from decimal import Decimal
-
-from django.contrib.auth.models import Group
 
 from jobs.forms import JobAccessForm
-from jobs.models import Job, CutUnit
-
-
-cut_units = CutUnit.objects.all()
-
-
-@pytest.fixture
-def create_user_and_job(django_user_model):
-    user = django_user_model.objects.create(
-        username='albic',
-        email='a@a.pl',
-        password='zaq12wsx',
-    )
-    user.groups.add(Group.objects.get(name='musicians'))
-    now_ = datetime.datetime.now(tz=datetime.timezone.utc)
-    job = Job.objects.create(
-        owner=user,
-        title='Rictusempra',
-        instrument='guitar',
-        music_style='rock',
-        description='loremipsum',
-        cut=Decimal(100),
-        cut_unit=random.choice(cut_units),
-        event_start=now_ + datetime.timedelta(hours=1),
-        event_end=now_ + datetime.timedelta(hours=3),
-    )
-    return user, job
 
 
 @pytest.mark.django_db
